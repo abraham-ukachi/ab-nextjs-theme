0ef012ecd284ffc5772fe8847f01b31603f674d0
abraham-ukachi <abraham.ukachi@laplateforme.io>
2024-07-30 16:58:08 +0200

    :fire: Update types/index.d.ts
    Create & use Colors interface

--- types/index.d.ts
+++ types/index.d.ts
@@ -50,12 +50,161 @@
 
 
 
+
+
+////////////////////////////
+/// PALETTES - interface ///
+////////////////////////////
+
+export interface Palettes {
+  primary: Record<string, string>;
+  secondary: Record<string, string>;
+  tertiary: Record<string, string>;
+  error?: Record<string, string>;
+  neutral: Record<string, string>;
+  neutralVariant: Record<string, string>;
+}
+
+
+
+
+///////////////////////////////
+/// CORE COLORS - interface ///
+///////////////////////////////
+
+export interface CoreColors {
+  primary: string;
+  secondary: string;
+  tertiaty: string;
+  error: string;
+  neutral: string;
+  ['neutral-variant']|neutralVariant?: string;
+}
+
+
+
+/////////////////////////////////
+/// SCHEME COLORS - interface ///
+/////////////////////////////////
+
+/**
+ * Scheme Colors - interface
+ *
+ * @description The color scheme of the abElements theme. 
+ * Supported schemes are `light`, `dark`, `lightM`, `lightH`, `darkM`, `darkH`. 
+ * where `M` stands for `Medium` contrast and `H` stands for `High` contrast.
+ */
+export interface SchemeColors {
+
+  // Primary
+  primary: string;
+  surfaceTint: string;
+  onPrimary: string;
+  primaryContainer: string;
+  onPrimaryContainer: string;
+  // Secondary
+  secondary: string;
+  onSecondary: string;
+  secondaryContainer: string;
+  onSecondaryContainer: string;
+  // Tertiary
+  tertiary: string;
+  onTertiary: string;
+  tertiaryContainer: string;
+  onTertiaryContainer: string;
+  // Error
+  error: string;
+  onError: string;
+  errorContainer: string;
+  onErrorContainer: string;
+  // Background
+  background: string;
+  onBackground: string;
+  // Surface
+  surface: string;
+  onSurface: string;
+  surfaceVariant: string;
+  onSurfaceVariant: string;
+  // Outline
+  outline: string;
+  outlineVariant: string;
+  // Shadow + Scrim
+  shadow: string;
+  scrim: string;
+  // Inverse
+  inverseSurface: string;
+  inverseOnSurface: string;
+  inversePrimary: string;
+
+  // Fixed - Primary
+  primaryFixed: string;
+  onPrimaryFixed: string;
+  primaryFixedDim: string;
+  onPrimaryFixedVariant: string;
+  // Fixed - Secondary
+  secondaryFixed: string;
+  onSecondaryFixed: string;
+  secondaryFixedDim: string;
+  onSecondaryFixedVariant: string;
+  // Fixed - Tertiary
+  tertiaryFixed: string;
+  onTertiaryFixed: string;
+  tertiaryFixedDim: string;
+  onTertiaryFixedVariant: string;
+  // Fixed - Surface
+  surfaceDim: string;
+  surfaceBright: string;
+  surfaceContainerLowest: string;
+  surfaceContainerLow: string;
+  surfaceContainer: string;
+  surfaceContainerHigh: string;
+  surfaceContainerHighest: string;
+  
+}
+
+
+
+
+//////////////////////////
+/// COLORS - interface ///
+//////////////////////////
+
+// Colors - interface
+export interface Colors extends SchemeColors {
+  seed: string;
+  core: CoreColors;
+
+  // TODO: classic: SchemeColors;
+  // TODO: classicM: SchemeColors;
+  // TODO: classicH: SchemeColors;
+
+  // light + medium & high contrast
+  light: SchemeColors;
+  lightM?: SchemeColors;
+  lightH?: SchemeColors;
+  
+  // dark + medium & high contrast
+  dark: SchemeColors;
+  darkM: SchemeColors;
+  darkH: SchemeColors;
+
+  palettes: Palettes;
+}
+
+
+
+
+
+
+
 ////////////////////////////
 /// AB THEME - interface ///
 ////////////////////////////
 
 export interface Theme {
-  colors: any;
+  colors: Colors;
   typography: any;
   styles: any;
 }
+
+
